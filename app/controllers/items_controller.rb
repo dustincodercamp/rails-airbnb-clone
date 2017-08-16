9b4366c9078542d8d450196b6075a4f09e1fcfa7
Dustin Cotcamp <dustincodercamp@gmail.com>
2017-08-16 13:01:48 -0300

    initial work

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,13 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+    @items = Item.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@items) do |item, marker|
+      marker.lat item.latitude
+      marker.lng item.longitude
+      # marker.infowindow render_to_string(partial: "/items/map_box", locals: { item: item })
+    end
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815153236) do
+ActiveRecord::Schema.define(version: 20170816143553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20170815153236) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
   end
 
   create_table "users", force: :cascade do |t|
