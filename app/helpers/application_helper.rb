660ba058406121913dfbb3db4f63b94778b4fb67
Dustin Cotcamp <dustincodercamp@gmail.com>
2017-08-18 17:03:13 -0300

    Added some default images and humanized the size and categories.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def avatar_url(image)
     image || "http://placehold.it/30x30"
   end
+
+  def item_owner_url(image)
+    image || "http://saxonyfineclothing.com/wp-content/uploads/2011/03/TAILOR_1-934x1024.jpg"
+  end
+
+  def item_url(category)
+    # return item.image if item.image.present?
+    if category.to_i == 1
+      "http://promtasticorganiser.weebly.com/uploads/1/6/7/9/16795080/7681625_orig.jpg"
+    else
+      "http://www.cheshireweddinguide.co.uk/wp-content/uploads/2014/11/Men_in_suits.jpg"
+    end
+  end
 end
